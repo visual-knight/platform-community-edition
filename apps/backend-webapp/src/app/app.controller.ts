dcaee1dfa7982124082ec1905a1ea26090f125d1
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-04-22 01:55:10 +0200

    chore: fix backend deployments

--- apps/backend-webapp/src/app/app.controller.ts
+++ apps/backend-webapp/src/app/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('webapp')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
