ca38e5ea73566c31026ac314fe76ea117b0003fb
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-08-23 13:13:57 +0200

    create core shared and layout module

--- apps/visual-knight/src/app/app.module.ts
+++ apps/visual-knight/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './modules/core/core.module';
+import { LayoutModule } from './modules/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, CoreModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
