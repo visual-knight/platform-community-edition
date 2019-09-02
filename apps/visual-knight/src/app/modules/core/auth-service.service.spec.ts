344e0f93f35402dcadb87f60a9f3427ba95c9533
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-09-02 19:49:18 +0400

    add auth logout on 401

--- apps/visual-knight/src/app/modules/core/auth-service.service.spec.ts
+++ apps/visual-knight/src/app/modules/core/auth-service.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthServiceService } from './auth-service.service';
+import { AuthService } from './auth-service.service';
 
 describe('AuthServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: AuthServiceService = TestBed.get(AuthServiceService);
+    const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
 });
