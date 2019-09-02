f3090fb4b1466ebab5cc5b60c68399e4dbb4ba0a
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-09-02 15:47:34 +0300

    add material and clipboard

--- apps/visual-knight/src/app/modules/variation/components/modals/delete-variation/delete-variation.component.ts
+++ apps/visual-knight/src/app/modules/variation/components/modals/delete-variation/delete-variation.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
-@Component ({
+@Component({
   selector: 'visual-knight-delete-variation',
   templateUrl: './delete-variation.component.html',
   styleUrls: ['./delete-variation.component.scss']
