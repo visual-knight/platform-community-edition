b46fbe3478802765fa6627871180b7f292471648
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-09-03 09:07:14 +0400

    fix service injection

--- apps/visual-knight/src/app/modules/core/core.module.ts
+++ apps/visual-knight/src/app/modules/core/core.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from './auth-service.service';
-import { TestHelperService } from './services/test-helper.service';
 import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [],
-  providers: [AuthService, TestHelperService],
   imports: [CommonModule, GraphQLModule]
 })
 export class CoreModule {}
