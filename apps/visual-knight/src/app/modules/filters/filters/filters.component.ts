20f8926d0c216ec72a4a51f3ab5a7ceefe359d0a
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-09-16 00:30:07 +0400

    feat: add browser, device and filter service

--- apps/visual-knight/src/app/modules/filters/filters/filters.component.ts
+++ apps/visual-knight/src/app/modules/filters/filters/filters.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { ProjectType, TestSessionType } from '../../core/types';
+import { Observable, of, combineLatest } from 'rxjs';
+import { ProjectType } from '../../core/types';
+import { Device } from '../../shared/device.model';
+import { Browser } from '../../shared/browser.model';
+import { map, tap, pluck } from 'rxjs/operators';
+import { FiltersService } from '../services/filters.service';
 
 @Component({
   selector: 'visual-knight-filters',
@@ -21,15 +25,84 @@ export class FiltersComponent implements OnInit {
   panelOpenState = false;
 
   projectList$: Observable<ProjectType[]>;
+  browserList$: Observable<any[]> = of(Browser.getBrowserList());
+  deviceList$: Observable<any[]> = of(Device.getDeviceList());
+  testSessionStateList$: Observable<
+    ('ACCEPTED' | 'DECLINED' | 'PENDING' | 'UNRESOLVED')[]
+  > = of(['ACCEPTED', 'DECLINED', 'PENDING', 'UNRESOLVED']);
 
-  activeFilterCount$: Observable<number>;
-  browserList$: Observable<any[]>;
-  deviceList$: Observable<any[]>;
-  testSessionStateList$: Observable<TestSessionType[]>;
+  filters$ = combineLatest(
+    this.filtersService.testNameFilter,
+    this.filtersService.browserFilter,
+    this.filtersService.deviceFilter,
+    this.filtersService.projectFilter,
+    this.filtersService.testStateFilter,
+    this.filtersService.customFilter
+  ).pipe(
+    tap(
+      ([
+        testNameFilter,
+        browserFilter,
+        deviceFilter,
+        projectFilter,
+        testStateFilter,
+        customFilter
+      ]) => {
+        this.filterForm.setValue({
+          testNameFilter,
+          browserFilter,
+          deviceFilter,
+          projectFilter,
+          testStateFilter,
+          customFilter
+        });
+      }
+    )
+  );
 
-  constructor(private formBuilder: FormBuilder) {}
+  activeFilterCount$: Observable<number> = this.filters$.pipe(
+    map(
+      ([
+        testNameFilter,
+        browserFilter,
+        deviceFilter,
+        projectFilter,
+        testStateFilter,
+        customFilter
+      ]) =>
+        projectFilter.length +
+        browserFilter.length +
+        deviceFilter.length +
+        (testNameFilter ? 1 : 0) +
+        (customFilter ? 1 : 0) +
+        testStateFilter.length
+    )
+  );
 
-  ngOnInit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private filtersService: FiltersService
+  ) {}
+
+  ngOnInit() {
+    this.filterForm.valueChanges.subscribe(
+      ({
+        testNameFilter,
+        browserFilter,
+        deviceFilter,
+        projectFilter,
+        testStateFilter,
+        customFilter
+      }) => {
+        this.filtersService.testNameFilter.next(testNameFilter);
+        this.filtersService.browserFilter.next(browserFilter);
+        this.filtersService.deviceFilter.next(deviceFilter);
+        this.filtersService.projectFilter.next(projectFilter);
+        this.filtersService.testStateFilter.next(testStateFilter);
+        this.filtersService.customFilter.next(customFilter);
+      }
+    );
+  }
 
   clearFilters() {}
 }
