831e70b64757fd4f8e20b31fe28b663c9318ab16
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-10-03 00:45:30 +0400

    feat: add header

--- apps/visual-knight/src/app/modules/layout/layout.module.ts
+++ apps/visual-knight/src/app/modules/layout/layout.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [SharedModule, CommonModule, RouterModule],
   exports: [HeaderComponent]
 })
-export class LayoutModule { }
+export class LayoutModule {}
