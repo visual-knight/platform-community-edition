318689d5bca23ef4d8b4d60a8cfabde27ae672c8
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2019-10-03 23:33:56 +0400

    feat: add auth canActivate guard fucntion

--- apps/visual-knight/src/app/app-routing.module.ts
+++ apps/visual-knight/src/app/app-routing.module.ts
@@ -16,18 +16,21 @@ const routes: Routes = [
   {
     path: 'project',
     canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/project/project.module').then(m => m.ProjectModule)
   },
   {
     path: 'test',
     canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/test/test.module').then(m => m.TestModule)
   },
   {
     path: 'variation',
     canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/variation/variation.module').then(
         m => m.VariationModule
--- apps/visual-knight/src/app/modules/core/guards/auth.guard.ts
+++ apps/visual-knight/src/app/modules/core/guards/auth.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router } from '@angular/router';
+import { CanLoad, Router, CanActivate } from '@angular/router';
 import { AuthService } from '../auth-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanLoad, CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canLoad(): boolean {
+    if (!this.authService.authenticated) {
+      this.router.navigate(['/user']);
+      return false;
+    }
+    return true;
+  }
+
+  canActivate(): boolean {
     if (!this.authService.authenticated) {
       this.router.navigate(['/user']);
       return false;
