610750644c095a09b3567095b2f3bfe7e4edbd22
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 00:00:01 +0100

    static.controller.spec.ts fixed

--- apps/api/src/app/shared/static/static.controller.spec.ts
+++ apps/api/src/app/shared/static/static.controller.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StaticController } from './static.controller';
+import { CloudProviderService } from '@visual-knight/api-interface';
 
 describe('Static Controller', () => {
   let controller: StaticController;
+  let cloudProviderService: CloudProviderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StaticController],
+      providers: [CloudProviderService]
     }).compile();
 
     controller = module.get<StaticController>(StaticController);
+    cloudProviderService = module.get<CloudProviderService>(CloudProviderService);
   });
 
   it('should be defined', () => {
