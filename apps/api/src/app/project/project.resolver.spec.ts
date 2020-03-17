ce16f7da8cb89f9c69e1b28ba0c240f049589664
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 00:22:48 +0100

    project.resolver.spec.ts fixed

--- apps/api/src/app/project/project.resolver.spec.ts
+++ apps/api/src/app/project/project.resolver.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectResolver } from './project.resolver';
+import { ProjectService } from './project.service';
+import {
+  PhotonService,
+  CloudProviderService
+} from '@visual-knight/api-interface';
 
 describe('ProjectResolver', () => {
   let resolver: ProjectResolver;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectResolver],
+      providers: [
+        ProjectResolver,
+        ProjectService,
+        PhotonService,
+        { provide: 'DB_URI', useValue: 'mock_db_url' }
+      ]
     }).compile();
     resolver = module.get<ProjectResolver>(ProjectResolver);
   });
