926d30f6cec9b36541e55f77f3008a4b51ce61db
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 00:19:37 +0100

    test.service.spec.ts fixed

--- apps/api/src/app/test/test.service.spec.ts
+++ apps/api/src/app/test/test.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestService } from './test.service';
+import {
+  PhotonService,
+  CloudProviderService
+} from '@visual-knight/api-interface';
 
 describe('TestService', () => {
   let service: TestService;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TestService],
+      providers: [
+        TestService,
+        PhotonService,
+        CloudProviderService,
+        { provide: 'DB_URI', useValue: 'mock_db_url' }
+      ]
     }).compile();
     service = module.get<TestService>(TestService);
   });
