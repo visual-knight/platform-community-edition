99031c0a5d9ce8a748ed33f8523fa529ef358ca3
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 00:14:55 +0100

    test.resolver.spec.ts fixed

--- apps/api/src/app/test/test.resolver.spec.ts
+++ apps/api/src/app/test/test.resolver.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestResolver } from './test.resolver';
+import { TestService } from './test.service';
+import {
+  PhotonService,
+  CloudProviderService
+} from '@visual-knight/api-interface';
+import { PrismaClient } from '@generated/photonjs';
 
 describe('TestResolver', () => {
   let resolver: TestResolver;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TestResolver],
+      providers: [
+        TestResolver,
+        TestService,
+        PhotonService,
+        CloudProviderService,
+        PrismaClient,
+        { provide: 'DB_URI', useValue: 'mock_db_url' }
+      ]
     }).compile();
     resolver = module.get<TestResolver>(TestResolver);
   });
