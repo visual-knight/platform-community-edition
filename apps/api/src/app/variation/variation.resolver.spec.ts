5288daa1c909f441e6f5db95d03d7a734e7dcaab
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 15:33:04 +0100

    variation.resolver.spec.ts fixed

--- apps/api/src/app/variation/variation.resolver.spec.ts
+++ apps/api/src/app/variation/variation.resolver.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VariationResolver } from './variation.resolver';
+import { VariationService } from './variation.service';
 
 describe('VariationResolver', () => {
   let resolver: VariationResolver;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VariationResolver],
+      providers: [
+        VariationResolver,
+        { provide: VariationService, useValue: {} }
+      ]
     }).compile();
     resolver = module.get<VariationResolver>(VariationResolver);
   });
