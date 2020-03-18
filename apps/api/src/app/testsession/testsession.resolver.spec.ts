c7af9c2ab29d47b3c4fe440b6da17d804d22ad33
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 14:39:54 +0100

    testsession.resolver.spec.ts fixed

--- apps/api/src/app/testsession/testsession.resolver.spec.ts
+++ apps/api/src/app/testsession/testsession.resolver.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestsessionResolver } from './testsession.resolver';
+import { TestsessionService } from './testsession.service';
+import {
+  PhotonService,
+  CloudProviderService
+} from '@visual-knight/api-interface';
 
 describe('TestsessionResolver', () => {
   let resolver: TestsessionResolver;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TestsessionResolver],
+      providers: [
+        TestsessionResolver,
+        TestsessionService,
+        CloudProviderService,
+        PhotonService,
+        { provide: 'DB_URI', useValue: 'mock_db_url' }
+      ]
     }).compile();
     resolver = module.get<TestsessionResolver>(TestsessionResolver);
   });
