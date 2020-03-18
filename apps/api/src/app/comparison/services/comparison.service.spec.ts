068ba6505933d7d34602ab5610a76b6d9d7d1369
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 15:37:54 +0100

    comparison.service.spec.ts fixed

--- apps/api/src/app/comparison/services/comparison.service.spec.ts
+++ apps/api/src/app/comparison/services/comparison.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ComparisonService } from './comparison.service';
+import {
+  PhotonService,
+  CloudProviderService
+} from '@visual-knight/api-interface';
 
 describe('ComparisonService', () => {
   let service: ComparisonService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ComparisonService]
+      providers: [
+        ComparisonService,
+        { provide: PhotonService, useValue: {} },
+        { provide: CloudProviderService, useValue: {} }
+      ]
     }).compile();
     service = module.get<ComparisonService>(ComparisonService);
   });
