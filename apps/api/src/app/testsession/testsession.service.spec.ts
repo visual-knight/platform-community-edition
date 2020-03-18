0f72b1a972baf051be6a9d9e270f05f79027efb0
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-18 14:41:19 +0100

    testsession.service.spec.ts fixed

--- apps/api/src/app/testsession/testsession.service.spec.ts
+++ apps/api/src/app/testsession/testsession.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TestsessionService } from './testsession.service';
+import {
+  PhotonService,
+  CloudProviderService
+} from '@visual-knight/api-interface';
 
 describe('TestsessionService', () => {
   let service: TestsessionService;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TestsessionService],
+      providers: [
+        TestsessionService,
+        CloudProviderService,
+        PhotonService,
+        { provide: 'DB_URI', useValue: 'mock_db_url' }
+      ]
     }).compile();
     service = module.get<TestsessionService>(TestsessionService);
   });
