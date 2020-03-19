77c57d441f9d11cd197c06262b769809af552c6b
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:37:02 +0100

    test.service.spec.ts fix

--- apps/visual-knight/src/app/modules/test/services/test.service.spec.ts
+++ apps/visual-knight/src/app/modules/test/services/test.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TestService } from './test.service';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('TestService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [ApolloTestingModule]
+    })
+  );
 
   it('should be created', () => {
     const service: TestService = TestBed.get(TestService);
