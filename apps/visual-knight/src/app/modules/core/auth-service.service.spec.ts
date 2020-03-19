2799bf25659d14155eaa730cce75822598ebcab9
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:12:43 +0100

    auth-service.service.spec.ts fixed

--- apps/visual-knight/src/app/modules/core/auth-service.service.spec.ts
+++ apps/visual-knight/src/app/modules/core/auth-service.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth-service.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('AuthServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, ApolloTestingModule]
+    })
+  );
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
