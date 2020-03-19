f368dbe1ff10701933c3877dc68878af30612ea8
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 18:12:44 +0100

    profile.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/user/components/profile/profile.component.spec.ts
+++ apps/visual-knight/src/app/modules/user/components/profile/profile.component.spec.ts
@@ -1,6 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatFormFieldModule,
+  MatCardModule,
+  MatSnackBarModule,
+  MatInputModule,
+  MatTableModule,
+  MatTabsModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatMenuModule,
+  MatBadgeModule
+} from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProfileComponent } from './profile.component';
+import { AccountManagementComponent } from '../account-management/account-management.component';
+import { UserManagementComponent } from '../user-management/user-management.component';
+import { VerificationBoxComponent } from '../verification-box/verification-box.component';
+import { ClipboardModule } from 'ngx-clipboard';
+import { of } from 'rxjs';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -8,14 +28,37 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ProfileComponent,
+        AccountManagementComponent,
+        UserManagementComponent,
+        VerificationBoxComponent
+      ],
+      imports: [
+        MatCardModule,
+        MatTabsModule,
+        MatTabsModule,
+        MatTooltipModule,
+        ClipboardModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatTableModule,
+        MatIconModule,
+        MatMenuModule,
+        RouterTestingModule,
+        ApolloTestingModule,
+        MatBadgeModule,
+        MatSnackBarModule,
+        BrowserAnimationsModule,
+        MatInputModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
+    component.userList$ = of([]);
     fixture.detectChanges();
   });
 
