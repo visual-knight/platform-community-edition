0ee78bf47e7112b097486028e37347958acf3413
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:04:22 +0100

    user.service.spec.ts fixed

--- apps/visual-knight/src/app/modules/user/services/user.service.spec.ts
+++ apps/visual-knight/src/app/modules/user/services/user.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [ApolloTestingModule]
+    })
+  );
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
