565b5b445622e39d5161a145c4b149e59d731f5a
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:30:27 +0100

    project.component.spec.ts fix

--- apps/visual-knight/src/app/modules/project/project.component.spec.ts
+++ apps/visual-knight/src/app/modules/project/project.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectComponent } from './project.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  MatCardModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatDialogModule,
+  MatSnackBarModule
+} from '@angular/material';
+import { ClipboardModule } from 'ngx-clipboard';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
@@ -8,9 +18,18 @@ describe('ProjectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectComponent ]
-    })
-    .compileComponents();
+      declarations: [ProjectComponent],
+      imports: [
+        RouterTestingModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatIconModule,
+        ClipboardModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        ApolloTestingModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
