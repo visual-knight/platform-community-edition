7cad4b7e190014af3a673ed27a86c8bb3ac49071
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:23:11 +0100

    project-add.component.spec.ts fix

--- apps/visual-knight/src/app/modules/project/components/modals/add-new-project/project-add.component.spec.ts
+++ apps/visual-knight/src/app/modules/project/components/modals/add-new-project/project-add.component.spec.ts
@@ -1,18 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddProjectModalComponent } from './project-add.component';
+import {
+  MatInputModule,
+  MatFormFieldModule,
+  MatDialogRef
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddComponent', () => {
   let component: AddProjectModalComponent;
   let fixture: ComponentFixture<AddProjectModalComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [AddProjectModalComponent]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddProjectModalComponent],
+      providers: [{ provide: MatDialogRef, useValue: {} }],
+      imports: [
+        MatInputModule,
+        FormsModule,
+        MatFormFieldModule,
+        BrowserAnimationsModule
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddProjectModalComponent);
