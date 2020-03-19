b8a48ffac9f22932c890c8c3bd7e04ac448b90c0
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 17:48:07 +0100

    login.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/user/components/login/login.component.spec.ts
+++ apps/visual-knight/src/app/modules/user/components/login/login.component.spec.ts
@@ -1,5 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatFormFieldModule,
+  MatProgressBarModule,
+  MatCardModule,
+  MatSnackBarModule,
+  MatInputModule
+} from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,7 +18,18 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [
+        MatFormFieldModule,
+        MatCardModule,
+        ReactiveFormsModule,
+        MatProgressBarModule,
+        RouterTestingModule,
+        MatInputModule,
+        MatSnackBarModule,
+        ApolloTestingModule,
+        BrowserAnimationsModule
+      ]
     }).compileComponents();
   }));
 
