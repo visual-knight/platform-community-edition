24985b273c4d5159172c4050b4de37b88bfc16b2
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 18:24:24 +0100

    os-icon.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/shared/modules/icon/os-icon/os-icon.component.spec.ts
+++ apps/visual-knight/src/app/modules/shared/modules/icon/os-icon/os-icon.component.spec.ts
@@ -1,21 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTooltipModule } from '@angular/material';
 import { BrowserIconComponent } from '../browser-icon/browser-icon.component';
 
 describe('BrowserIconComponent', () => {
   let component: BrowserIconComponent;
   let fixture: ComponentFixture<BrowserIconComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [BrowserIconComponent]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BrowserIconComponent],
+      imports: [MatTooltipModule]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BrowserIconComponent);
     component = fixture.componentInstance;
+    component.browser = '';
     fixture.detectChanges();
   });
 
