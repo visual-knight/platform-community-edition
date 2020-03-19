f441465871dcc81c49be85d17dd0c4cf7b47be80
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 16:53:19 +0100

    test-variation-list.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/variation/components/test-variation-list/test-variation-list.component.spec.ts
+++ apps/visual-knight/src/app/modules/variation/components/test-variation-list/test-variation-list.component.spec.ts
@@ -1,18 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatCardModule, MatDialogModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ScreenshotImagePipe } from '../../../shared/pipes/screenshot-image.pipe';
 import { VariationListComponent } from './test-variation-list.component';
+import { APP_BASE_HREF } from '@angular/common';
+import {
+  ApolloTestingModule,
+} from 'apollo-angular/testing';
 
 describe('TestVariationListComponent', () => {
   let component: VariationListComponent;
   let fixture: ComponentFixture<VariationListComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [VariationListComponent]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [VariationListComponent, ScreenshotImagePipe],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+      imports: [
+        MatCardModule,
+        MatDialogModule,
+        RouterTestingModule,
+        ApolloTestingModule
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VariationListComponent);
