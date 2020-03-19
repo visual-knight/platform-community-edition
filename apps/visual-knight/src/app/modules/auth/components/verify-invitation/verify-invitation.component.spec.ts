d0de7bde3099233cd685135ca2ceef229e9ae98b
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:08:41 +0100

    verify-invitation.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/auth/components/verify-invitation/verify-invitation.component.spec.ts
+++ apps/visual-knight/src/app/modules/auth/components/verify-invitation/verify-invitation.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VerifyInvitationComponent } from './verify-invitation.component';
+import { MatInputModule, MatTooltipModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('VerifyInvitationComponent', () => {
   let component: VerifyInvitationComponent;
@@ -8,9 +13,16 @@ describe('VerifyInvitationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VerifyInvitationComponent ]
-    })
-    .compileComponents();
+      declarations: [VerifyInvitationComponent],
+      imports: [
+        MatInputModule,
+        MatTooltipModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        ApolloTestingModule,
+        BrowserAnimationsModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
