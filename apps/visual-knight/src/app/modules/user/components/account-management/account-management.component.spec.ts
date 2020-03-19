c43d013c22d6c27025197675b11d5531b5b8ca36
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 17:35:09 +0100

    account-management.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/user/components/account-management/account-management.component.spec.ts
+++ apps/visual-knight/src/app/modules/user/components/account-management/account-management.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import {
+  MatFormFieldModule,
+  MatTooltipModule,
+  MatInputModule
+} from '@angular/material';
 import { AccountManagementComponent } from './account-management.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 describe('AccountManagementComponent', () => {
   let component: AccountManagementComponent;
@@ -8,14 +16,28 @@ describe('AccountManagementComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccountManagementComponent ]
-    })
-    .compileComponents();
+      declarations: [AccountManagementComponent],
+      imports: [
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatTooltipModule,
+        ApolloTestingModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AccountManagementComponent);
     component = fixture.componentInstance;
+    component.user$ = of({
+      id: null,
+      email: '',
+      apiKey: '',
+      active: null,
+      role: ''
+    });
     fixture.detectChanges();
   });
 
