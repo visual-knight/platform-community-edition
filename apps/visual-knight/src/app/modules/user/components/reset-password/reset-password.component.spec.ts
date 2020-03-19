62b01ae743c4c096a98680cfeb3715cca1e56067
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 18:46:07 +0100

    reset-password.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/user/components/reset-password/reset-password.component.spec.ts
+++ apps/visual-knight/src/app/modules/user/components/reset-password/reset-password.component.spec.ts
@@ -1,19 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MatCardModule,
+  MatProgressBarModule,
+  MatInputModule,
+  MatTooltipModule,
+  MatSnackBarModule
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResetPasswordComponent } from './reset-password.component';
 
-import { ForgotPasswordComponent } from './forgot-password.component';
-
-describe('ForgotPasswordComponent', () => {
-  let component: ForgotPasswordComponent;
-  let fixture: ComponentFixture<ForgotPasswordComponent>;
+describe('ResetPasswordComponent', () => {
+  let component: ResetPasswordComponent;
+  let fixture: ComponentFixture<ResetPasswordComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ForgotPasswordComponent]
+      declarations: [ResetPasswordComponent],
+      imports: [
+        MatCardModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatProgressBarModule,
+        MatTooltipModule,
+        MatSnackBarModule,
+        ApolloTestingModule,
+        BrowserAnimationsModule
+      ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ForgotPasswordComponent);
+    fixture = TestBed.createComponent(ResetPasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
