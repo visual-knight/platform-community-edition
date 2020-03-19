e520ebee9031ceb99163d3b19e7408fc74738654
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 13:58:01 +0100

    variation.service.spec.ts fixed

--- apps/visual-knight/src/app/modules/variation/services/variation.service.spec.ts
+++ apps/visual-knight/src/app/modules/variation/services/variation.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
 import { VariationService } from './variation.service';
+import { Apollo } from 'apollo-angular';
 
 describe('VariationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [VariationService, { provide: Apollo, useValue: {} }]
+    })
+  );
 
   it('should be created', () => {
     const service: VariationService = TestBed.get(VariationService);
