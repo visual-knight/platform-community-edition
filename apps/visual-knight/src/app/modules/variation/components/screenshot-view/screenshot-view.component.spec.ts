78a1bf7702a05d537f92620339470982af0b29f9
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 14:30:07 +0100

    screenshot-view.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/variation/components/screenshot-view/screenshot-view.component.spec.ts
+++ apps/visual-knight/src/app/modules/variation/components/screenshot-view/screenshot-view.component.spec.ts
@@ -1,18 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material';
 import { ScreenshotViewComponent } from './screenshot-view.component';
+import { ScreenshotImagePipe } from '../../../shared/pipes/screenshot-image.pipe';
 
 describe('ScreenshotViewComponent', () => {
   let component: ScreenshotViewComponent;
   let fixture: ComponentFixture<ScreenshotViewComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [ScreenshotViewComponent]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ScreenshotViewComponent, ScreenshotImagePipe],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ],
+      imports: [MatDialogModule]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ScreenshotViewComponent);
