6eae15c3fb1c275358e1138fd75051a32f65b7e5
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:25:43 +0100

    delete-modal.component.spec.ts fix

--- apps/visual-knight/src/app/modules/project/components/modals/delete-modal/delete-modal.component.spec.ts
+++ apps/visual-knight/src/app/modules/project/components/modals/delete-modal/delete-modal.component.spec.ts
@@ -1,18 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteModalComponent } from './delete-modal.component';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material';
 
 describe('DeleteModalComponent', () => {
   let component: DeleteModalComponent;
   let fixture: ComponentFixture<DeleteModalComponent>;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [DeleteModalComponent]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DeleteModalComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ],
+      imports: [MatDialogModule]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DeleteModalComponent);
