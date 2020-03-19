730d3d213a3c1d260a75d5ed0d1592d6add90a34
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 18:40:04 +0100

    test.component.spec.ts

--- apps/visual-knight/src/app/modules/test/test.component.spec.ts
+++ apps/visual-knight/src/app/modules/test/test.component.spec.ts
@@ -1,5 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatTableModule,
+  MatIconModule,
+  MatMenuModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { TestListComponent } from '../test/components/test-list/test-list.component';
+import { FiltersComponent } from '../filters/filters/filters.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponent } from './test.component';
 
 describe('TestComponent', () => {
@@ -8,9 +22,21 @@ describe('TestComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestComponent ]
-    })
-    .compileComponents();
+      declarations: [TestComponent, TestListComponent, FiltersComponent],
+      imports: [
+        MatTableModule,
+        MatIconModule,
+        MatMenuModule,
+        RouterTestingModule,
+        MatExpansionModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ApolloTestingModule,
+        BrowserAnimationsModule,
+        MatInputModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
