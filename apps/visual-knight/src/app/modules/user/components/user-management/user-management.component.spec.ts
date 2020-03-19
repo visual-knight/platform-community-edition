b131a7939efbb4c82f3cc6a05eb7a9b8b5866aea
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-19 19:01:48 +0100

    user-management.component.spec.ts fixed

--- apps/visual-knight/src/app/modules/user/components/user-management/user-management.component.spec.ts
+++ apps/visual-knight/src/app/modules/user/components/user-management/user-management.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserManagementComponent } from './user-management.component';
+import {
+  MatTableModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatMenuModule,
+  MatInputModule
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('UserManagementComponent', () => {
   let component: UserManagementComponent;
@@ -8,14 +18,22 @@ describe('UserManagementComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserManagementComponent ]
-    })
-    .compileComponents();
+      declarations: [UserManagementComponent],
+      imports: [
+        MatTableModule,
+        MatIconModule,
+        MatMenuModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserManagementComponent);
     component = fixture.componentInstance;
+    component.users$ = of([]);
     fixture.detectChanges();
   });
 
