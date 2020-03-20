1cbf7bd9a60ae7026cb47cb74bc56a3ccc9bc03e
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-20 18:44:08 +0100

    comparison.service.ts | updateImageData covered with test

--- apps/api/src/app/comparison/services/comparison.service.spec.ts
+++ apps/api/src/app/comparison/services/comparison.service.spec.ts
@@ -4,21 +4,69 @@ import {
   PhotonService,
   CloudProviderService
 } from '@visual-knight/api-interface';
+import { TestSessionState } from '@generated/photonjs';
 
 describe('ComparisonService', () => {
   let service: ComparisonService;
+  let photonService: PhotonService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ComparisonService,
-        { provide: PhotonService, useValue: {} },
+        {
+          provide: PhotonService,
+          useValue: {
+            testSession: {
+              update: jest.fn()
+            }
+          }
+        },
         { provide: CloudProviderService, useValue: {} }
       ]
     }).compile();
     service = module.get<ComparisonService>(ComparisonService);
+    photonService = module.get<PhotonService>(PhotonService);
   });
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('updateImageData', () => {
+    const params = {
+      testSessionId: 'testSessionId',
+      imageKey: 'testSession.imageKey',
+      state: TestSessionState.UNRESOLVED,
+      diffImageKey: 'diffImageKey',
+      diffBaselineRef: 'diffBaselineRef',
+      misMatchPercentage: 15,
+      isSameDimensions: true
+    };
+
+    it('should update image data', () => {
+      photonService.testSession.update = jest
+        .fn()
+        .mockResolvedValueOnce(params);
+
+      service['updateImageData'](
+        params.testSessionId,
+        params.imageKey,
+        params.state,
+        params.diffImageKey,
+        params.misMatchPercentage,
+        params.isSameDimensions
+      );
+
+      expect(photonService.testSession.update).toHaveBeenCalledWith({
+        where: { id: params.testSessionId },
+        data: {
+          imageKey: params.imageKey,
+          diffImageKey: params.diffImageKey,
+          misMatchPercentage: params.misMatchPercentage,
+          isSameDimensions: params.isSameDimensions,
+          state: params.state
+        }
+      });
+    });
   });
 });
