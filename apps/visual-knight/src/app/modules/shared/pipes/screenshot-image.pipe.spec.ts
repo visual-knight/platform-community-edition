ce74ae714f1a1bc28710c69ef8ad9557726083ad
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-28 00:47:08 +0100

    screenshot-image.pipe. return placeholder if no image

--- apps/visual-knight/src/app/modules/shared/pipes/screenshot-image.pipe.spec.ts
+++ apps/visual-knight/src/app/modules/shared/pipes/screenshot-image.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { ScreenshotImagePipe } from './screenshot-image.pipe';
+import { environment } from '../../../../environments/environment';
 
 describe('ScreenshotImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ScreenshotImagePipe();
-    expect(pipe).toBeTruthy();
+  let pipe: ScreenshotImagePipe;
+
+  beforeAll(() => {
+    pipe = new ScreenshotImagePipe();
+  });
+
+  it('should return no image from server', () => {
+    const image = 'image.png';
+    expect(pipe.transform(image)).toBe(
+      environment.apiEndpoint + environment.screenshotsPath + image
+    );
+  });
+
+  it('should return no image asset', () => {
+    expect(pipe.transform(null)).toBe('../../../assets/images/no-img.jpg');
   });
 });
