db642686d6855767914fb65c651cf9eb19bad289
Pavlo Strunkin <pashidlos@gmail.com>
2020-03-31 11:56:29 +0200

    test added

--- apps/visual-knight/src/app/modules/test/components/test-list/test-list.component.spec.ts
+++ apps/visual-knight/src/app/modules/test/components/test-list/test-list.component.spec.ts
@@ -7,6 +7,10 @@ import {
 import { RouterTestingModule } from '@angular/router/testing';
 import { ApolloTestingModule } from 'apollo-angular/testing';
 import { TestListComponent } from './test-list.component';
+import { of } from 'rxjs';
+import { TestDataFragment } from '../../../core/types';
+import { TestsDataSource } from '../../test.datasource';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TestListComponent', () => {
   let component: TestListComponent;
@@ -20,7 +24,8 @@ describe('TestListComponent', () => {
         MatIconModule,
         RouterTestingModule,
         MatMenuModule,
-        ApolloTestingModule
+        ApolloTestingModule,
+        BrowserAnimationsModule
       ]
     }).compileComponents();
   }));
@@ -33,5 +38,31 @@ describe('TestListComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render not empty test list', () => {
+    const testList: TestDataFragment[] = [
+      {
+        id: '12',
+        name: 'name 1',
+        variations: [
+          {
+            id: '33',
+            browserName: 'Chrome',
+            deviceName: 'Device name',
+            createdAt: Date.now(),
+            testSessions: []
+          }
+        ],
+        project: {
+          id: '44'
+        }
+      }
+    ];
+    component.dataSource = new TestsDataSource(of(testList));
+
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
   });
 });
