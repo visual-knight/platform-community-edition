79a2448bece709a94e66c8e541cba35343ea32f5
Pavlo Strunkin <pashidlos@gmail.com>
2020-04-01 10:32:10 +0200

    tests are added

--- apps/visual-knight/src/app/modules/filters/filters/filters.component.spec.ts
+++ apps/visual-knight/src/app/modules/filters/filters/filters.component.spec.ts
@@ -10,10 +10,40 @@ import {
 import { ReactiveFormsModule } from '@angular/forms';
 import { ApolloTestingModule } from 'apollo-angular/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FiltersService } from '../services/filters.service';
+import { ProjectType } from '../../core/types';
+import { Browser } from '../../shared/browser.model';
+import { Device } from '../../shared/device.model';
+import { BehaviorSubject, of } from 'rxjs';
+import { ProjectService } from '../../project/services/project.service';
+import { By } from '@angular/platform-browser';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
+  let filterService: FiltersService;
+  let projectService: ProjectService;
+
+  const emptyFilterState = {
+    testNameFilter: '',
+    browserFilter: [],
+    deviceFilter: [],
+    projectFilter: [],
+    testStateFilter: []
+  };
+
+  const usedFilterState = {
+    testNameFilter: 'some test name',
+    browserFilter: ['Chrome'],
+    deviceFilter: ['Windows'],
+    projectFilter: [
+      {
+        id: '12',
+        name: 'some project name'
+      }
+    ],
+    testStateFilter: ['ACCEPTED']
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -32,11 +62,72 @@ describe('FiltersComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FiltersComponent);
+    filterService = TestBed.get(FiltersService);
+    projectService = TestBed.get(ProjectService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('snapshot filter are empty', () => {
+    const filter = emptyFilterState;
+    filterService.setFilter(filter);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('snapshot filter used', () => {
+    const filter = usedFilterState;
+    filterService.setFilter(filter);
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  describe('onInit', () => {
+    it('should set form values according to filterService', () => {
+      const filter = usedFilterState;
+      filterService.setFilter(filter);
+
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      expect(component.filterForm.getRawValue()).toStrictEqual(filter);
+    });
+
+    it('should call filterService state on change', () => {
+      const filter = usedFilterState;
+      filterService.setFilter = jest.fn();
+
+      component.filterForm.setValue(filter);
+      fixture.detectChanges();
+
+      expect(filterService.setFilter).toHaveBeenCalledWith(filter);
+    });
+  });
+
+  describe('clearFilters', () => {
+    it('should call method on click', () => {
+      const filter = usedFilterState;
+      component.filterForm.setValue(filter);
+      component.clearFilters = jest.fn()
+
+      fixture.nativeElement.querySelector('#clearFilters').click()
+
+      expect(component.clearFilters).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set to default state', () => {
+      const filter = usedFilterState;
+      component.filterForm.setValue(filter);
+
+      component.clearFilters();
+
+      expect(component.filterForm.getRawValue()).toStrictEqual(emptyFilterState);
+    });
   });
 });
