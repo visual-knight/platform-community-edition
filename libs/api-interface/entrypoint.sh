bd11e06dcc51613ce51eb0247d66a57a4f17295f
Pavlo Strunkin <pavlo.strunkin@eu.jll.com>
2020-04-04 12:32:47 +0200

    migration is applied

--- libs/api-interface/entrypoint.sh
+++ libs/api-interface/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec /wait-for-it.sh $POSTGRES_SERVER:$POSTGRES_PORT -- npx prisma migrate up --experimental
+exec /wait-for-it.sh $POSTGRES_SERVER:$POSTGRES_PORT -- prisma migrate up -c --auto-approve --experimental 
 # exec npx prisma2 migrate up --experimental
